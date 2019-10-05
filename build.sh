70c0690d6d418646a2780f6711f43fc145335125
vbcpascal <vbcpascal@126.com>
2019-10-05 16:17:03 +0800

    update build script

---
+++ build.sh
@@ -0,0 +1,34 @@
+CMAKE="${MAKE:-cmake}"
+
+MAKE="${MAKE:-make}"
+
+realpath() {
+    cd "$@" && pwd
+}
+
+SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+SRC_DIR="$(realpath ${SCRIPT_DIR}/)"
+BUILD_DIR="${SRC_DIR}/build/"
+
+echo "SRC_DIR=${SRC_DIR}"
+echo "BUILD_DIR=${BUILD_DIR}"
+
+die() {
+    echo "Error: $1" >&2
+    exit 1
+}
+
+if ! $MAKE --help &>/dev/null; then
+    die "GNU make is required."
+fi
+
+echo "Configuring project with $(which $CMAKE)..."
+
+rm -rf "${BUILD_DIR}" && mkdir "${BUILD_DIR}"
+
+pushd "${BUILD_DIR}"
+$CMAKE "${SRC_DIR}" || die "Failed to configure project"
+$MAKE -j$(nproc) || die "Failed to build project"
+popd
+
+echo "Build succeed."
