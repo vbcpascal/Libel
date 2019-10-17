092a7568fbbac3884b00d9d3a35db7049a76dcd6
Zhichao Guan <33946372+vbcpascal@users.noreply.github.com>
2019-10-17 17:26:58 +0800

    [fix] update ARP

--- tests/testARP.cpp
+++ tests/testARP.cpp
@@ -3,15 +3,17 @@
 #include "device.h"
 
 int main(int argc, char* argv[]) {
-  std::string devName, addr;
-  printf("please input a device name: ");
-  std::cin >> devName;
-  printf("please input an ip address: ");
-  std::cin >> addr;
+  printf("Usage: ./testARP en0 1.2.3.4\n");
+  char* devName = argv[argc - 2];
+  char* addr = argv[argc - 1];
+  printf(
+      "Try to get MAC address of \033[33;1m%s\033[0m, via "
+      "\033[33;1m%s\033[0m\n",
+      addr, devName);
 
   api::init();
   DeviceId id;
-  id = addDevice(devName.c_str());
+  id = addDevice(devName);
   if (id < 0) {
     LOG_ERR("Device not found: %s", argv[0]);
     return 0;
@@ -19,12 +21,13 @@ int main(int argc, char* argv[]) {
   Device::DevicePtr dev = Device::deviceMgr.getDevicePtr(id);
 
   ip_addr ip;
-  int res = inet_aton(addr.c_str(), &ip);
+  int res = inet_aton(addr, &ip);
   if (res <= 0) {
     LOG_ERR("Ip address error: %s", argv[2]);
     return 0;
   }
   Arp::arpMgr.getMacAddr(dev, ip, 5);
+  printf("MAC address: ");
   Printer::printMAC(Arp::arpMgr.ipMacMap[ip].addr);
 
   Device::deviceMgr.keepReceiving();
