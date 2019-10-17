7ab5f97a963de5ffc68d65420719c531a715b89d
Zhichao Guan <33946372+vbcpascal@users.noreply.github.com>
2019-10-17 16:33:28 +0800

    [fix] testARP

--- tests/testARP.cpp
+++ tests/testARP.cpp
@@ -3,17 +3,13 @@
 #include "device.h"
 
 int main(int argc, char* argv[]) {
-  if (argc <= 2) {
-    printf("please input a device name and an ip address.\n");
-    printf("example: ./testARP en0 1.2.3.4\n");
-  }
+  char devName[20], addr[25];
+  scanf("please input a device name: %s\n", devName);
+  scanf("please input an ip address: %s\n", addr);
 
   api::init();
   DeviceId id;
-  if (argc <= 1)
-    id = addDevice("en0");
-  else
-    id = addDevice(argv[1]);
+  id = addDevice(devName);
   if (id < 0) {
     LOG_ERR("Device not found: %s", argv[0]);
     return 0;
@@ -21,12 +17,7 @@ int main(int argc, char* argv[]) {
   Device::DevicePtr dev = Device::deviceMgr.getDevicePtr(id);
 
   ip_addr ip;
-  int res;
-  if (argc <= 2)
-    res = inet_aton("1.2.3.4", &ip);
-  else
-    res = inet_aton(argv[2], &ip);
-
+  int res = inet_aton(addr, &ip);
   if (res <= 0) {
     LOG_ERR("Ip address error: %s", argv[2]);
     return 0;
